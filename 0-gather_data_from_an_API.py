75ed5383ba5bf805bcd944ec9f14c966fdef8f7a
Kambai <kariukikambai@gmail.com>
2024-10-21 16:51:35 +0200

    task 0 done

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+id = int(sys.argv[1])
+todos_url = 'https://jsonplaceholder.typicode.com/todos/'
+users_url = 'https://jsonplaceholder.typicode.com/users/'
+
+def get_info(id):
+   completed_todos = []
+   todos_list = []
+   completed_titles = []
+   user_details = requests.get(f"{users_url}/{id}").json() 
+   todos = requests.get(todos_url).json()
+   for todo in todos:
+      if todo["userId"] == id:
+         todos_list.append(todo)
+      if todo["userId"] == id and todo["completed"] == True:
+         completed_todos.append(todo["title"])
+   
+   print(f"Employee {user_details["name"]} is done with tasks({len(completed_todos)}/{len(todos_list)}):")
+   for title in completed_todos:
+      print(f"  {title}")
+   
+#    Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+   
+get_info(id)
